53cbe52642599a71f7e80b81f83a658156326355
futebolmultimilionario <79179613+futebolmultimilionario@users.noreply.github.com>
2021-03-27 00:13:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
 	$bot_url    = "https://api.telegram.org/bot".$botToken;
 
 	if(array_key_exists('channel_post', $requisicao)){
-
 		if(!array_key_exists('sticker', $requisicao['channel_post']) && !array_key_exists('video', $requisicao['channel_post']) && !array_key_exists('poll', $requisicao['channel_post'])){
 			if(array_key_exists('caption', $requisicao['channel_post'])){
 				$texto = $requisicao['channel_post']['caption'];
@@ -20,10 +19,12 @@
 				$texto = $requisicao['channel_post']['text'];
 			} if(strpos(strtolower($texto), 'duplo green') === false && strpos(strtolower($texto), 'duplogreen') === false && strpos(strtolower($texto), 'green duplo') === false && strpos(strtolower($texto), 'greenduplo') === false && strpos(strtolower($texto), 'duplo') === false && strpos(strtolower($texto), 'relatório') === false && strpos(strtolower($texto), 'relatorio') === false && strpos(strtolower($texto), ' dg') === false){
 				$chat_id = $requisicao['channel_post']['chat']['id'];
+				if($chat_id=='-1001256582495'){
 				$msgid = $requisicao['channel_post']['message_id'];
 				$url = $bot_url."/copyMessage?chat_id=".$idchat."&from_chat_id=".$chat_id."&message_id=".$msgid;
 				$resposta = json_decode(file_get_contents($url), TRUE);
 				$msgid_rep = $resposta['result']['message_id'];
+				}
 			}
 		}
 		file_put_contents("idmsg.csv",$msgid.",".$msgid_rep.PHP_EOL,FILE_APPEND);
@@ -32,7 +33,7 @@
 	if(array_key_exists('edited_channel_post', $requisicao)){
 		$chat_id = $requisicao['edited_channel_post']['chat']['id'];
 		$msgid = $requisicao['edited_channel_post']['message_id'];
-
+		if($chat_id=='-1001256582495'){
 		if (($h = fopen("idmsg.csv", "r")) !== FALSE){
 			// Convert each line into the local $data variable
 			$i=0;
@@ -60,6 +61,7 @@
   			}
 			}
   			fclose($h);
+		}
 		}
 	}
 
