48ae5a6b4c6646c533c293a84769788f5937f5b5
futebolmultimilionario <79179613+futebolmultimilionario@users.noreply.github.com>
2021-03-29 12:29:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	$requisicao = json_decode($dados, TRUE);
 
 	$botToken = "1698766079:AAFM5MWB11wDVhiSVzulNCTDt8L6QHE_UNI";
+	$idchat2 = "-1001443215600";
 	$idchat = "-1001443215600";
 	$bot_url    = "https://api.telegram.org/bot".$botToken;
 
@@ -22,12 +23,15 @@
 				if($chat_id=='-1001459553477'){
 				$msgid = $requisicao['channel_post']['message_id'];
 				$url = $bot_url."/copyMessage?chat_id=".$idchat."&from_chat_id=".$chat_id."&message_id=".$msgid;
+				$url2 = $bot_url."/copyMessage?chat_id=".$idchat2."&from_chat_id=".$chat_id."&message_id=".$msgid;
 				$resposta = json_decode(file_get_contents($url), TRUE);
+				$resposta2 = json_decode(file_get_contents($url2), TRUE);
 				$msgid_rep = $resposta['result']['message_id'];
+				$msgid_rep2 = $resposta2['result']['message_id'];
 				}
 			}
 		}
-		file_put_contents("idmsg.csv",$msgid.",".$msgid_rep.PHP_EOL,FILE_APPEND);
+		file_put_contents("idmsg.csv",$msgid.",".$msgid_rep.",".$msgid_rep2.PHP_EOL,FILE_APPEND);
 	}
 
 	if(array_key_exists('edited_channel_post', $requisicao)){
@@ -45,14 +49,18 @@
 						$texto = $requisicao['edited_channel_post']['text'];
 						if(strpos(strtolower($texto), 'duplo green') === false && strpos(strtolower($texto), 'duplogreen') === false && strpos(strtolower($texto), 'green duplo') === false && strpos(strtolower($texto), 'greenduplo') === false && strpos(strtolower($texto), 'duplo') === false && strpos(strtolower($texto), 'relatório') === false && strpos(strtolower($texto), 'relatorio') === false && strpos(strtolower($texto), ' dg') === false){
 						$url = $bot_url."/editMessageText?chat_id=".$idchat."&message_id=".$teste[$i][1]."&text=".urlencode($texto);
+						$url2 = $bot_url."/editMessageText?chat_id=".$idchat2."&message_id=".$teste[$i][2]."&text=".urlencode($texto);
 						file_get_contents($url);
+						file_get_contents($url2);
 						}
 					} else {
 						$texto = $requisicao['edited_channel_post']['caption'];
 
 						if(strpos(strtolower($texto), 'duplo green') === false && strpos(strtolower($texto), 'duplogreen') === false && strpos(strtolower($texto), 'green duplo') === false && strpos(strtolower($texto), 'greenduplo') === false && strpos(strtolower($texto), 'duplo') === false && strpos(strtolower($texto), 'relatório') === false && strpos(strtolower($texto), 'relatorio') === false && strpos(strtolower($texto), ' dg') === false){
 						$url = $bot_url."/editMessageCaption?chat_id=".$idchat."&message_id=".$teste[$i][1]."&caption=".urlencode($texto);
+						$url2 = $bot_url."/editMessageCaption?chat_id=".$idchat2."&message_id=".$teste[$i][2]."&caption=".urlencode($texto);
 						file_get_contents($url);
+						file_get_contents($url2);
 						}
 					}
 					break;
