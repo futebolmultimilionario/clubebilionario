f6f71fb8de45639c22a84a44e16e040fc86c8b14
futebolmultimilionario <79179613+futebolmultimilionario@users.noreply.github.com>
2021-04-01 16:24:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,16 +13,16 @@
 	$idchat = "-1001443215600";
 	$bot_url    = "https://api.telegram.org/bot".$botToken;
 
-	if(array_key_exists('channel_post', $requisicao)){
-		if(!array_key_exists('sticker', $requisicao['channel_post']) && !array_key_exists('video', $requisicao['channel_post']) && !array_key_exists('poll', $requisicao['channel_post'])){
-			if(array_key_exists('caption', $requisicao['channel_post'])){
-				$texto = $requisicao['channel_post']['caption'];
+	if(array_key_exists('message', $requisicao)){
+		if(!array_key_exists('sticker', $requisicao['message']) && !array_key_exists('video', $requisicao['message']) && !array_key_exists('poll', $requisicao['message'])){
+			if(array_key_exists('caption', $requisicao['message'])){
+				$texto = $requisicao['message']['caption'];
 			} else {
-				$texto = $requisicao['channel_post']['text'];
+				$texto = $requisicao['message']['text'];
 			} if(strpos(strtolower($texto), 'duplo green') === false && strpos(strtolower($texto), 'duplogreen') === false && strpos(strtolower($texto), 'green duplo') === false && strpos(strtolower($texto), 'greenduplo') === false && strpos(strtolower($texto), 'duplo') === false && strpos(strtolower($texto), 'relatório') === false && strpos(strtolower($texto), 'relatorio') === false && strpos(strtolower($texto), ' dg') === false){
-				$chat_id = $requisicao['channel_post']['chat']['id'];
+				$chat_id = $requisicao['message']['chat']['id'];
 				if($chat_id=='-1001360346569'){
-				$msgid = $requisicao['channel_post']['message_id'];
+				$msgid = $requisicao['message']['message_id'];
 				$url = $bot_url."/copyMessage?chat_id=".$idchat."&from_chat_id=".$chat_id."&message_id=".$msgid;
 				$url2 = $bot_url."/copyMessage?chat_id=".$idchat2."&from_chat_id=".$chat_id."&message_id=".$msgid;
 				$url3 = $bot_url."/copyMessage?chat_id=".$idchat3."&from_chat_id=".$chat_id."&message_id=".$msgid;
@@ -39,19 +39,19 @@
 		
 	}
 
-	if(array_key_exists('edited_channel_post', $requisicao)){
-		$chat_id = $requisicao['edited_channel_post']['chat']['id'];
-		$msgid = $requisicao['edited_channel_post']['message_id'];
+	if(array_key_exists('edited_message', $requisicao)){
+		$chat_id = $requisicao['edited_message']['chat']['id'];
+		$msgid = $requisicao['edited_message']['message_id'];
 		if($chat_id=='-1001360346569'){
 		if (($h = fopen("idmsg.csv", "r")) !== FALSE){
 			// Convert each line into the local $data variable
 			$i=0;
-			if(!array_key_exists('sticker', $requisicao['edited_channel_post']) && !array_key_exists('video', $requisicao['edited_channel_post']) && !array_key_exists('poll', $requisicao['edited_channel_post'])){
+			if(!array_key_exists('sticker', $requisicao['edited_message']) && !array_key_exists('video', $requisicao['edited_message']) && !array_key_exists('poll', $requisicao['edited_message'])){
 			while (($data = fgetcsv($h, 1000, ",")) !== FALSE){
 				$teste[]=$data;
 				if($teste[$i][0] == $msgid){
-					if(!array_key_exists('photo', $requisicao['edited_channel_post'])){
-						$texto = $requisicao['edited_channel_post']['text'];
+					if(!array_key_exists('photo', $requisicao['edited_message'])){
+						$texto = $requisicao['edited_message']['text'];
 						if(strpos(strtolower($texto), 'duplo green') === false && strpos(strtolower($texto), 'duplogreen') === false && strpos(strtolower($texto), 'green duplo') === false && strpos(strtolower($texto), 'greenduplo') === false && strpos(strtolower($texto), 'duplo') === false && strpos(strtolower($texto), 'relatório') === false && strpos(strtolower($texto), 'relatorio') === false && strpos(strtolower($texto), ' dg') === false){
 						$url = $bot_url."/editMessageText?chat_id=".$idchat."&message_id=".$teste[$i][1]."&text=".urlencode($texto);
 						$url2 = $bot_url."/editMessageText?chat_id=".$idchat2."&message_id=".$teste[$i][2]."&text=".urlencode($texto);
@@ -61,7 +61,7 @@
 						file_get_contents($url3);
 						}
 					} else {
-						$texto = $requisicao['edited_channel_post']['caption'];
+						$texto = $requisicao['edited_message']['caption'];
 
 						if(strpos(strtolower($texto), 'duplo green') === false && strpos(strtolower($texto), 'duplogreen') === false && strpos(strtolower($texto), 'green duplo') === false && strpos(strtolower($texto), 'greenduplo') === false && strpos(strtolower($texto), 'duplo') === false && strpos(strtolower($texto), 'relatório') === false && strpos(strtolower($texto), 'relatorio') === false && strpos(strtolower($texto), ' dg') === false){
 						$url = $bot_url."/editMessageCaption?chat_id=".$idchat."&message_id=".$teste[$i][1]."&caption=".urlencode($texto);
